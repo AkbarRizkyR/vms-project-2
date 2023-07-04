866eecf0320b7bfb9e633dd23f249d10713b02a3
Akbar Rizky Rabbani <arrizki0098@gmail.com>
2023-07-04 17:57:00 +0700

    Header FIxing

--- src/components/body/Header.vue
+++ src/components/body/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="flex items-center justify-between py-4 px-6 border-b-4">
         <div>
-            <img src="@/assets/icons/logo_pln.png" alt="Logo">
+            <img src="@/assets/icons/logo_pln.png" alt="Logo" class="ml-44">
         </div>
-        <nav class="flex items-center space-x-7">
+        <nav class="flex items-center space-x-7 mr-20 ml-10">
             <ul class="flex space-x-4 font-sans font-semibold">
                 <!-- Add the menu items here -->
                 <li><a href="#">Home</a></li>
@@ -16,7 +16,7 @@
         </nav>
         <div>
             <!-- Replace 'Logo2' with your second logo -->
-            <img src="@/assets/icons/logo-eproc.png" alt="logo-eproc">
+            <img src="@/assets/icons/logo-eproc.png" alt="logo-eproc" class="mr-44">
         </div>
     </header>
 </template>
